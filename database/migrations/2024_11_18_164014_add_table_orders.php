6a04cb94a3beda1a3c0be56dc2055eca8bb709a4
Camilo Alarcon <camilo4750@gmail.com>
2024-11-18 16:11:14 -0500

    Fix name table

--- database/migrations/2024_11_18_164014_add_table_orders.php
+++ database/migrations/2024_11_18_164014_add_table_orders.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('orders_status', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->foreignId('supplier_id')->constrained('suppliers')->cascadeOnDelete();
@@ -33,6 +33,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('orders_status');
+        Schema::dropIfExists('orders');
     }
 };
