b7acc90db7e8f2bb70e8a9b4747c5645dc738865
Camilo Alarcon <camilo4750@gmail.com>
2024-11-19 15:49:42 -0500

    Add migrations for billings, taxes, billings status and billings details tables

---
+++ database/migrations/2024_11_19_202341_add_table_billings.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('billings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('vehicle_id')->constrained('vehicles');
+            $table->foreignId('service_id')->constrained('services');
+            $table->foreignId('billing_status_id')->nullable()->constrained('billing_status')->nullOnDelete();
+            $table->foreignId('taxe_id')->constrained('taxes');
+            $table->foreignId('payment_method_id')->constrained('payment_methods');
+            $table->decimal('total_amount',8,2)->nullable();
+            $table->timestamp('billing_date')->nullable();
+            $table->string('observation', 250)->nullable();
+            $table->foreignId('user_who_created_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignId('user_who_updated_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignId('user_who_deleted_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('billings');
+    }
+};
---
+++ database/migrations/2024_11_19_203239_add_table_billing_details.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('billing_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('billing_id')->constrained('billings');
+            $table->string('name', 250);
+            $table->integer('quantity');
+            $table->decimal('unit_price', 8, 2);
+            $table->decimal('total_price',8,2);
+            $table->foreignId('user_who_created_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignId('user_who_updated_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignId('user_who_deleted_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('billing_details');
+    }
+};
